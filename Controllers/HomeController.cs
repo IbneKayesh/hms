2e4b2aaa7debc4f3890ac7a8b442e262cc1c5491
Md. Ibne Kayesh <mis95@mis.prangroup.com>
2021-03-25 13:09:27 +0600

    first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,9 @@ namespace hms.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        private void test_git_hub()
+        {
+
+        }
     }
 }
