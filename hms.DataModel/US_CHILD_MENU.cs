7a60b364e4e6f84fa17b55ad64aa1fa8baf76a5f
MithunKumarCSE <mithunkumar71@gmail.com>
2021-03-25 17:29:18 +0600

    Class Update Complete

--- hms.DataModel/US_CHILD_MENU.cs
+++ hms.DataModel/US_CHILD_MENU.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,17 @@ namespace hms.DataModel
     public class US_CHILD_MENU : DEFAULT
     {
         public int ID { get; set; }
-        public int MODULE_ID { get; set; }
-        public int CHILD_ID { get; set; }
+
+        [Display(Name = "Child Name")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "{0} length is {2} between {1}")]
+        [Required(ErrorMessage = "{0} is required")]
+        public string CHILD_NAME { get; set; }
+
+        [Display(Name = "Module Name Bangla")]
+        [StringLength(20)]
+        public string CHILD_BN_NAME { get; set; }
+        public string CHILD_ICON { get; set; }
+        public string CONTROLLER_NAME { get; set; }
+        public string ACTION_NAME { get; set; }
     }
 }
