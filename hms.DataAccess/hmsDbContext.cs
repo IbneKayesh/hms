633117fcea5a5cfae5043e42fdc69e6025651d1c
MithunKumarCSE <mithunkumar71@gmail.com>
2021-03-25 17:34:34 +0600

    DbContext Update

--- hms.DataAccess/hmsDbContext.cs
+++ hms.DataAccess/hmsDbContext.cs
@@ -15,5 +15,9 @@ namespace hms.DataAccess
         public DbSet<US_MARITAIL_STATUS> US_MARITAIL_STATUS { get; set; }
         public DbSet<US_MODULE> US_MODULE { get; set; }
         public DbSet<US_RELIGION> US_RELIGION { get; set; }
+        public DbSet<US_BLOOD_GROUP> US_BLOOD_GROUP { get; set; }
+        public DbSet<US_ROLE> US_ROLE { get; set; }
+        public DbSet<US_TYPE> US_TYPE { get; set; }
+        public DbSet<US_USER_ROLE> US_USER_ROLE { get; set; }
     }
 }
