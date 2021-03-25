f3da4858bc4aaaddf4c8ad749491d411a7d2a29b
Md. Ibne Kayesh <mis95@mis.prangroup.com>
2021-03-25 18:17:38 +0600

    area added

--- hms/Startup.cs
+++ hms/Startup.cs
@@ -46,10 +46,17 @@ namespace hms
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {
+            { 
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=User}/{action=Index}/{id?}"
+                );
             });
         }
     }
