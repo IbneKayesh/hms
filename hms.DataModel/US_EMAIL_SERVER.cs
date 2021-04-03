90259f2f6fd7d640554eb88d838e7db851e13e1b
IBNE KAYESH <ibnekayesh91@outlook.com>
2021-04-03 16:00:39 +0600

    email models

---
+++ hms.DataModel/US_EMAIL_SERVER.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hms.DataModel
+{
+
+    public class US_EMAIL_SERVER
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "{0} length is {2} between {1}")]
+        [Required(ErrorMessage = "{0} is required")]
+
+        public string SERVER_IP { get; set; }
+
+        [Range(minimum: 0, maximum: 9999, ErrorMessage = "{0} length is {2} between {1}")]
+        [Required(ErrorMessage = "{0} is required")]
+        public int PORT_NO { get; set; }
+
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "{0} length is {2} between {1}")]
+        [Required(ErrorMessage = "{0} is required")]
+        public string DISPLAY_NAME { get; set; }
+
+        [StringLength(100, MinimumLength =3, ErrorMessage = "{0} length is {2} between {1}")]
+        [Required(ErrorMessage = "{0} is required")]
+        public string ACCOUNT_NAME { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} length is {2} between {1}")]
+        [Required(ErrorMessage = "{0} is required")]
+        public string ACCOUNT_CREDENTIAL { get; set; }
+    }
+}
