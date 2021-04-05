033c92162890702a0c36610b4ecf5141467a7c54
IBNE KAYESH <ibnekayesh91@outlook.com>
2021-04-05 22:13:19 +0600

    .

---
+++ hms.DataModel/ViewModels/HP_PRESCRIPTION_VM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hms.DataModel.ViewModels
+{
+    public class HP_PRESCRIPTION_VM
+    {
+        public DateTime PRESCRIPTION_DATE { get; set; }
+        public int HOSPITAL_ID { get; set; }
+    }
+}
