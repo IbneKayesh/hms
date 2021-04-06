3e5d3775b553c4ac54fb324d490c0fcfedaed5ee
IBNE KAYESH <ibnekayesh91@outlook.com>
2021-04-06 22:21:38 +0600

    Pages Source Protection JS added

---
+++ hms/wwwroot/js/pageprotection.js
@@ -0,0 +1,12 @@
+﻿//kayesh 8 apr 2021
+$(document).bind("contextmenu", function (e) {
+    e.preventDefault();
+});
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) {
+        return false;//Prevent from F12
+    }
+    else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+        return false; //Prevent from ctrl+shift+i
+    }
+});
